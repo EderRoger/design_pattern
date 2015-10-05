6c991793c32ea4822b2657f264da0cffc0e9bf07
Eder Roger <eder.roger.souza@gmail.com>
2015-10-05 20:45:27 -0300

    add some more condiments to diagram

--- src/decorator/Decaf.java
+++ src/decorator/Decaf.java
@@ -1,5 +1,8 @@
 package decorator;
 
+import java.io.CharArrayReader;
+import java.io.IOException;
+
 /**
  * Created by eder on 04/10/15.
  */
@@ -13,4 +16,5 @@ public class Decaf extends Beverage {
     double cost() {
         return 1.05;
     }
+
 }
---
+++ src/decorator/Mocha.java
@@ -0,0 +1,23 @@
+package decorator;
+
+/**
+ * Created by eder on 05/10/15.
+ */
+public class Mocha extends CondimentDecorator {
+
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    String getDescripton() {
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    @Override
+    double cost() {
+        return .20 + beverage.cost();
+    }
+}
