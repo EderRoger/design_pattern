a76b3bb6a45e6c8a2f161fa82d61452607c3be06
Eder Roger <eder.roger.souza@gmail.com>
2015-10-17 22:18:03 -0300

    more classes to demostrate command

---
+++ src/command/NoCommand.java
@@ -0,0 +1,11 @@
+package command;
+
+/**
+ * Created by eder on 17/10/15.
+ */
+public class NoCommand implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/command/RemoteControl.java
@@ -0,0 +1,20 @@
+package command;
+
+/**
+ * Created by eder on 17/10/15.
+ */
+public class RemoteControl {
+    Command[] onCommands;
+    Command[] offCommands;
+
+    public RemoteControl() {
+        onCommands = new Command[7];
+        offCommands = new Command[7];
+
+        Command noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
+    }
+}
