7d05f6625bc50e0d4d4cf6b8e4b16b00c7b7c971
Eder Roger <eder.roger.souza@gmail.com>
2015-10-19 21:27:20 -0200

    add undo test

--- test/src/command/RemoteUndoTest.java
+++ test/src/command/RemoteUndoTest.java
@@ -8,40 +8,21 @@ import org.junit.Test;
 public class RemoteUndoTest {
 
     @Test
-    public void testRemote(){
+    public void testRemoteUndo(){
         RemoteControl remoteControl = new RemoteControl();
 
         Light livingRoomLight = new Light("Living Room");
-        Light kitchenLight = new Light("Kitchen");
-        GarageDoor garageDoor = new GarageDoor(livingRoomLight);
-        Stereo stereo = new Stereo("Living Room");
 
         LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
         LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
-        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
-        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
-
-        GarageDoorUpCommand garageDoorUpCommand = new GarageDoorUpCommand(garageDoor);
-        GarageDoorDownCommand garageDoorDownCommand = new GarageDoorDownCommand(garageDoor);
-
-        StereoOnWithCDCommand stereoOnWithCDCommand = new StereoOnWithCDCommand(stereo);
-        StereoOffCommand stereoOffCommand = new StereoOffCommand(stereo);
 
         remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
-        remoteControl.setCommand(2, stereoOnWithCDCommand, stereoOffCommand);
-        remoteControl.setCommand(3, garageDoorUpCommand, garageDoorDownCommand);
 
         System.out.println(remoteControl);
 
         remoteControl.onButtonWasPushed(0);
-        remoteControl.offButtonWasPushed(0);
-        remoteControl.onButtonWasPushed(1);
-        remoteControl.offButtonWasPushed(1);
-        remoteControl.onButtonWasPushed(2);
-        remoteControl.offButtonWasPushed(2);
-        remoteControl.onButtonWasPushed(3);
-        remoteControl.offButtonWasPushed(3);
+        remoteControl.undoButtonWasPushed();
+        System.out.println(remoteControl);
     }
 
 }
