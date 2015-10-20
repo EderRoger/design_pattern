531e157a6f405479a0f4c506a537a738c479d64e
Eder Roger <eder.roger.souza@gmail.com>
2015-10-20 20:51:17 -0200

    test on marco command

--- src/command/MacroCommand.java
+++ src/command/MacroCommand.java
@@ -19,6 +19,8 @@ public class MacroCommand implements Command {
 
     @Override
     public void undo() {
-
+        for (int i = 0; i < commands.length; i++) {
+            commands[i].undo();
+        }
     }
 }
