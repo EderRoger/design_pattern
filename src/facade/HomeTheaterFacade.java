06f4812e89c5bea5eef417cceabefff9be1cc982
Eder Roger <eder.roger.souza@gmail.com>
2015-10-24 18:38:30 -0200

    add classe to facade pattern

---
+++ src/facade/HomeTheaterFacade.java
@@ -0,0 +1,26 @@
+package facade;
+
+/**
+ * Created by eder on 24/10/15.
+ */
+public class HomeTheaterFacade {
+    Amplifier amp;
+    Tuner tuner;
+    DvdPlayer dvd;
+    CdPlayer cd;
+    Projector projector;
+    TheaterLights lights;
+    Screen screen;
+    PopcornPopper popper;
+
+    public HomeTheaterFacade(Amplifier amp, Tuner tuner, DvdPlayer dvd, CdPlayer cd, Projector projector, TheaterLights lights, Screen screen, PopcornPopper popper) {
+        this.amp = amp;
+        this.tuner = tuner;
+        this.dvd = dvd;
+        this.cd = cd;
+        this.projector = projector;
+        this.lights = lights;
+        this.screen = screen;
+        this.popper = popper;
+    }
+}
---
+++ src/facade/PopcornPopper.java
@@ -0,0 +1,7 @@
+package facade;
+
+/**
+ * Created by eder on 24/10/15.
+ */
+public class PopcornPopper {
+}
