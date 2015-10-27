05bc813a4a14eb9c7557ea4d142aab4d156d836b
Eder Roger <eder.roger.souza@gmail.com>
2015-10-26 23:03:41 -0200

    add classes to template method

---
+++ src/template_method/Coffe.java
@@ -0,0 +1,30 @@
+package template_method;
+
+/**
+ * Created by eder on 26/10/15.
+ */
+public class Coffe {
+    void prepareRecipe(){
+        boilWater();
+        brewCoffeGrinds();
+        pourInCup();
+        addSugarAndMilk();
+    }
+
+    public void addSugarAndMilk() {
+
+    }
+
+    public void pourInCup() {
+
+    }
+
+    public void brewCoffeGrinds() {
+
+    }
+
+    public void boilWater() {
+
+    }
+
+}
