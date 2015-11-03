21e90b834c3fa1273296f2b360bc946aef5f0d43
Eder Roger <eder.roger.souza@gmail.com>
2015-11-03 21:51:12 -0200

    add more classes to example iterators

--- src/iterator/PancakeHouseMenu.java
+++ src/iterator/PancakeHouseMenu.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
  * Created by eder on 29/10/15.
  */
 public class PancakeHouseMenu {
-    ArrayList menuItens;
+    ArrayList menuItems;
 
     public PancakeHouseMenu() {
-        menuItens  = new ArrayList();
+        menuItems = new ArrayList();
         addItem("K&B's Pancake Breakfast", "Pancakes with scramble eggs, and toast", true, 2.99);
         addItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
         addItem("Blueberry Pancake's", "Pancakes made with fresh blueberries", true, 3.49);
@@ -18,10 +18,10 @@ public class PancakeHouseMenu {
 
     private void addItem(String name, String description, boolean vegetarian, double price) {
         MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
-        menuItens.add(menuItem);
+        menuItems.add(menuItem);
     }
 
-    public ArrayList getMenuItens() {
-        return menuItens;
+    public Iterator createIterator(){
+        return  new PancakeHouseMenuIterator(menuItems);
     }
 }
---
+++ src/iterator/PancakeHouseMenuIterator.java
@@ -0,0 +1,28 @@
+package iterator;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eder on 03/11/15.
+ */
+public class PancakeHouseMenuIterator implements Iterator {
+
+    ArrayList menuItems;
+    int position = 0;
+
+    public PancakeHouseMenuIterator(ArrayList menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !(position >= menuItems.size() || menuItems.get(position) == null);
+    }
+
+    @Override
+    public Object next() {
+        MenuItem menuItem = (MenuItem) menuItems.get(position);
+        position++;
+        return menuItem;
+    }
+}
