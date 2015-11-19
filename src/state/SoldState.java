db673ccee59854efc3dff869601a382af104b817
Eder Roger <eder.roger.souza@gmail.com>
2015-11-19 20:44:25 -0200

    criate a sold state

---
+++ src/state/SoldState.java
@@ -0,0 +1,26 @@
+package state;
+
+/**
+ * Created by eder on 19/11/15.
+ */
+public class SoldState implements State {
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+
+    }
+}
