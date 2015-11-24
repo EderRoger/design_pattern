c65e1f3edc949080eb04bf623417f3fa8dfc28a9
Eder Roger <eder.roger.souza@gmail.com>
2015-11-23 23:11:22 -0200

    add some refactoring

--- src/state/HasQuarterState.java
+++ src/state/HasQuarterState.java
@@ -1,14 +1,18 @@
 package state;
 
+import java.util.Random;
+
 /**
  * Created by eder on 19/11/15.
  */
 public class HasQuarterState implements State {
+    Random randomWinner = new Random(System.currentTimeMillis());
     GumballMachine gumballMachine;
 
     public HasQuarterState(GumballMachine gumballMachine) {
         this.gumballMachine = gumballMachine;
     }
+
     @Override
     public void insertQuarter() {
         System.out.println("You cant insert another quarter");
@@ -23,7 +27,12 @@ public class HasQuarterState implements State {
     @Override
     public void turnCrank() {
         System.out.println("you turned...");
-        gumballMachine.setState(gumballMachine.getSoldState());
+        int winner = randomWinner.nextInt(10);
+        if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+            gumballMachine.setState(gumballMachine.getWinnerState());
+        } else {
+            gumballMachine.setState(gumballMachine.getSoldState());
+        }
     }
 
     @Override
